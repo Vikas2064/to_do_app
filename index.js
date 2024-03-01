42fc5a1d9a852c55cb9ec1535cd321d35a85801a
Vikas2064 <vikask200202@gmail.com>
2024-03-01 12:04:52 +0530

    understanding the cors error and solving it on the bakcend side

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const port= 5000;
+const bodyParser = require('body-parser')
+const makeTodo=require('./router')
+const path = require('path')
+const cors= require('cors');
+app.use(cors()) ;
+app.use('/todo',makeTodo);
+app.use(bodyParser.json());
 
 
-app.get('/',(req,res)=>{
-    res.send("this is main page")
-})
-
+// app.get("/",(req,res)=>{
+//     res.sendFile(path.join(__dirname,"index.html"))
+// })
 app.listen(port,()=>{
     console.log(`listening on the port ${port}`);
-})
+})   
